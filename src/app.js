bc2b8e0f574416e7dce46d5100156830e71716f6
El Miri Martin <bsbebgcst@gmail.com>
2026-03-25 14:55:05 +0100

    névjeggyel kiegészítve

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+/*
+* File: app.js
+* Author: El Miri Martin
+* Copyright: 2026, El Miri Martin
+* Group: Szoft I-N
+* Date: 2026.03.25
+* Github: https://github.com/Martin91625/
+* Licenc: MIT
+*/
+
 import calcBmi from "./calcBmi.js"
 
 const doc = {
--- src/calcBmi.js
+++ src/calcBmi.js
@@ -1,3 +1,12 @@
+/*
+* File: calcBmi.js
+* Author: El Miri Martin
+* Copyright: 2026, El Miri Martin
+* Group: Szoft I-N
+* Date: 2026.03.25
+* Github: https://github.com/Martin91625/
+* Licenc: MIT
+*/
 function calcBmi (mass, height) {
     return mass / height**2;
 }
